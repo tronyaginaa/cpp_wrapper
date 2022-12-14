c4fbe2d33421616ae44b1a5779e8ccd1c160f4da
tronyaginaa <tronyaginaa@gmail.com>
2022-12-14 20:46:54 +0300

    apdate main.cpp

--- wrapper/main.cpp
+++ wrapper/main.cpp
@@ -9,18 +9,19 @@
 #include "subjects.hpp"
 
 int main(int argc, const char * argv[]) {
-    
     Subject1 subj_first;
-    Wrapper wrapper_first(&subj_first, &Subject1::f2, {{"arg1", 0}, {"arg2", 0}});
-    Engine engine_first;
-    engine_first.registerCommand(&wrapper_first, "command1");
-    std::cout << engine_first.execute("command1", {{"arg1", 4}, {"arg2", 5}}) << std::endl;
-    
     Subject2 subj_second;
+    
+    Wrapper wrapper_first(&subj_first, &Subject1::f2, {{"arg1", 0}, {"arg2", 0}});
     Wrapper wrapper_second(&subj_second, &Subject2::f3, {{"arg1", 0}, {"arg2", 0}, {"arg3", 0}});
-    Engine engine_second;
-    engine_second.registerCommand(&wrapper_second, "command");
-    std::cout << engine_second.execute("command", {{"arg1", 4}, {"arg2", 5}, {"arg3", 6}}) << std::endl;
+    
+    Engine engine;
+    
+    engine.registerCommand(&wrapper_first, "command1");
+    std::cout << engine.execute("command1", {{"arg1", 4}, {"arg2", 5}}) << std::endl;
+    
+    engine.registerCommand(&wrapper_second, "command");
+    std::cout << engine.execute("command", {{"arg1", 4}, {"arg2", 5}, {"arg3", 6}}) << std::endl;
     
     return 0;
 }
